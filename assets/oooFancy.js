8b7cd7678ca8a77d7c3778b891b0f34b4c0e3d2d
michellebu <michellebu@berkeley.edu>
2011-10-22 00:32:16 -0700

    strinnngy

--- assets/oooFancy.js
+++ assets/oooFancy.js
@@ -1,13 +1,4 @@
   $(window).load(function() {
-    $('#pushThisButton').click(function() {
-      $('#semiRelevantInfo').slideToggle('slow', function() {
-        $('#editor, #containsCodeEditor').css({ 'height' : '500' });
-        var editor = ace.edit("editor");
-        var PythonMode = require("ace/mode/python").Mode;
-        editor.getSession().setMode(new PythonMode());
-        $('#showForm').animate({ 'opacity' : '1' });
-      });
-    });
     
     $('.bigBlack').hover(function() {
       $(this).stop().animate({'background-color': '#67ad4b'});
