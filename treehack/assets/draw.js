76dd4c69df0825b99f89eea1aaf035258b9a4be5
Doboy <Doboy@Huans-MacBook-Pro.local>
2011-11-07 15:24:00 -0800

    clean up a bit

--- treehack/assets/draw.js
+++ treehack/assets/draw.js
@@ -1,6 +1,6 @@
 depthVars = function( things, d ) {
     return Math.max.apply( Math, things.map( function(e) { return depthVars( e, d + 1 ) } ));
-}
+};
 
 countVars = function( things ) {
     var ret = 0;
@@ -13,9 +13,7 @@ countVars = function( things ) {
     return ret;
 };
 
-draw = function( things, base, length, first ) {
-    var dx = 10;
-    var dy = 10;
+draw = function( things, base, length ) {
     count = countVars( things );
     var increment = 2 * Math.PI / count;
     var angle = Math.random()*2 * Math.PI;
@@ -23,15 +21,15 @@ draw = function( things, base, length, first ) {
         if ( kind != 'vars' && kind != 'methods' && kind != 'classes' ) continue;
         for( var name in things[ kind ] ) { var item = things[ kind ][ name ];
             if( count == 1 ) {
-                var X = base.x - dx ;
-                var Y = base.y - dy ;
+                var X = base.x + 10 * Math.cos( angle );
+                var Y = base.y + 10 * Math.sin( angle );
                 var f = 1;
             }
             else {
                 var X = base.x + length * Math.cos( angle );
                 var Y = base.y + length * Math.sin( angle );
-                var f = .4
-                    }
+                var f = .6;
+            }
             var tit = 'nothing';
             //if ( item.doc ) { tit = item.doc }
             item.el =
@@ -39,7 +37,7 @@ draw = function( things, base, length, first ) {
                 addClass( kind + 'Fruit' ).attr('title', tit ).css({left: X, top: Y}).toggle( function(event){
                         var data = $(this).data('info');
                         if ( data.fruit ) {
-                            draw( data.fruit, data.base, data.length, false );
+                            draw( data.fruit, data.base, data.length );
                         }
                     }, function(event){
                         var data = $(this).data('info');
@@ -74,5 +72,5 @@ drawInit = function( all ) {
     var base = { x : 521/2, y : 658/ 2 };
     var length = base.x / 2;
     
-    draw( all, base, length, true );
+    draw( all, base, length );
 };
