e86d5521d795db5dd3a417e681966b38b14344eb
Doboy <Doboy@Huans-MacBook-Pro.local>
2011-11-07 11:01:08 -0800

    foo

--- treehack/views.py
+++ treehack/views.py
@@ -11,17 +11,17 @@ from django.template import RequestContext, Template, Context
 #secure all evals!
 
 def GiveGetCode(request):
-     if request.is_ajax():
-          if request.method == "POST":
-               parsed = HACK(request.POST["code"])
-               thiscode = Code(rawcode = request.POST["code"],
-                               serialcode = parsed)
-               thiscode.save()
-               return HttpResponse(simplejson.dumps({"ID":thiscode.id, "parsed":parsed}), 'application/json')
-          if request.method == "GET":
-               thiscode = Code.objects.get(id=request.GET["ID"])
-               return HttpResponse(simplejson.dumps({"raw":thiscode.rawcode, "parsed":HACK(thiscode.rawcode)}), 'application/json')
-
+     if request.method == "POST":
+          parsed = HACK(request.POST["code"])
+          thiscode = Code(rawcode = request.POST["code"],
+                          serialcode = parsed)
+          thiscode.save()
+          return HttpResponse(simplejson.dumps({"ID":thiscode.id, "parsed":parsed}), 'application/json')
+     elif request.method == "GET":
+          thiscode = Code.objects.get(id=request.GET["ID"])
+          return HttpResponse(simplejson.dumps({"raw":thiscode.rawcode, "parsed":HACK(thiscode.rawcode)}), 'application/json')
+     else:
+          return HttpResponse( simplejson.dumps( request ) )
 
 def DisplayCode(request, ID):
      return render_to_response('results.html', context_instance=RequestContext(request))
