d6d39edb36c6ef0d0e445c160d962511464399c5
Doboy <Doboy@Huans-MacBook-Pro.local>
2011-11-07 13:23:08 -0800

    db crap

--- treehack/views.py
+++ treehack/views.py
@@ -1,6 +1,6 @@
 # Create your views here.
 from django.http import HttpResponse, HttpResponseRedirect
-from models import *
+from treehack.models import *
 #from forms import *
 from django.core import serializers
 from django.utils import simplejson
@@ -14,14 +14,13 @@ def GiveGetCode(request):
      if request.is_ajax():
           if request.method == "POST":
                parsed = HACK(request.POST["code"])
-               # commented until I set up db
-               # thiscode = Code(rawcode = request.POST["code"],
-               # serialcode = parsed)
-               # thiscode.save()
-               return HttpResponse(simplejson.dumps({"ID": 0 , "parsed":parsed}), 'application/json')
-          # elif request.method == "GET":
-               # thiscode = Code.objects.get(id=request.GET["ID"])
-               # return HttpResponse(simplejson.dumps({"raw":thiscode.rawcode, "parsed":HACK(thiscode.rawcode)}), 'application/json')
+               thiscode = Code(rawcode = request.POST["code"],
+                               serialcode = parsed)
+               thiscode.save()
+               return HttpResponse(simplejson.dumps({"ID": thiscode.id , "parsed":parsed}), 'application/json')
+          elif request.method == "GET":
+               thiscode = Code.objects.get(id=request.GET["ID"])
+               return HttpResponse(simplejson.dumps({"raw":thiscode.rawcode, "parsed":HACK(thiscode.rawcode)}), 'application/json')
           else:
                return HttpResponse( simplejson.dumps( {'4':3} ) )
      else:
