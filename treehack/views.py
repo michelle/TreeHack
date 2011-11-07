53e9e16d1b9345715214dc8ab94f6430f101d3cd
Doboy <Doboy@Huans-MacBook-Pro.local>
2011-11-07 12:49:56 -0800

    csrf crap

--- treehack/views.py
+++ treehack/views.py
@@ -13,14 +13,15 @@ from django.template import RequestContext, Template, Context
 def GiveGetCode(request):
      if request.is_ajax():
           if request.method == "POST":
-               parsed = HACK(request.POST["code"])
-               thiscode = Code(rawcode = request.POST["code"],
-                               serialcode = parsed)
-               thiscode.save()
-               return HttpResponse(simplejson.dumps({"ID":thiscode.id, "parsed":parsed}), 'application/json')
-          elif request.method == "GET":
-               thiscode = Code.objects.get(id=request.GET["ID"])
-               return HttpResponse(simplejson.dumps({"raw":thiscode.rawcode, "parsed":HACK(thiscode.rawcode)}), 'application/json')
+               # disabling this for now, before I set up db
+               # parsed = HACK(request.POST["code"])
+               # thiscode = Code(rawcode = request.POST["code"],
+               serialcode = parsed)
+               # thiscode.save()
+               return HttpResponse(simplejson.dumps({"ID": 0 , "parsed":parsed}), 'application/json')
+          # elif request.method == "GET":
+               # thiscode = Code.objects.get(id=request.GET["ID"])
+               # return HttpResponse(simplejson.dumps({"raw":thiscode.rawcode, "parsed":HACK(thiscode.rawcode)}), 'application/json')
           else:
                return HttpResponse( simplejson.dumps( {'4':3} ) )
      else:
