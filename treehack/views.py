cab8a418b8547a59b824912393a3e129a6961e4b
Doboy <Doboy@Huans-MacBook-Pro.local>
2011-11-07 13:04:38 -0800

    db crap

--- treehack/views.py
+++ treehack/views.py
@@ -14,13 +14,14 @@ def GiveGetCode(request):
      if request.is_ajax():
           if request.method == "POST":
                parsed = HACK(request.POST["code"])
-               thiscode = Code(rawcode = request.POST["code"],
-               serialcode = parsed)
-               thiscode.save()
+               # commented until I set up db
+               # thiscode = Code(rawcode = request.POST["code"],
+               # serialcode = parsed)
+               # thiscode.save()
                return HttpResponse(simplejson.dumps({"ID": 0 , "parsed":parsed}), 'application/json')
-          elif request.method == "GET":
-               thiscode = Code.objects.get(id=request.GET["ID"])
-               return HttpResponse(simplejson.dumps({"raw":thiscode.rawcode, "parsed":HACK(thiscode.rawcode)}), 'application/json')
+          # elif request.method == "GET":
+               # thiscode = Code.objects.get(id=request.GET["ID"])
+               # return HttpResponse(simplejson.dumps({"raw":thiscode.rawcode, "parsed":HACK(thiscode.rawcode)}), 'application/json')
           else:
                return HttpResponse( simplejson.dumps( {'4':3} ) )
      else:
